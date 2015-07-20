06bb59745b5d88b67bbaeab4e36068526d3d307f
Sebastien Binet <binet@cern.ch>
2015-07-20 15:08:02 +0200

    doc: add godoc.org documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+// fedid provides an identity federator service enabling user connection thru
+// different identity providers.
+//
+// For more informations, see:
+//   https://github.com/lodygens/fedid/blob/master/doc/fedid.md
+//
+package main
